7cdbbff7d57c3592af14c13239965681a82601db
Michael Lipman <michael.b.lipman@gmail.com>
2013-07-31 01:52:48 -0700

    Added home page route to explore/index

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ Majors2::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
+  root :to => 'explore#index'
   match ':controller(/:action(/:id))(.:format)'
 end
