a335de5777b2c512853a942770fcfcd231c862bf
Michael Lipman <michael.b.lipman@gmail.com>
2013-09-08 22:54:55 -0700

    Mostly whitespace, adding max and min units and clearing old migrations. Setting the stage for major req and chunk changes, 3

---
+++ db/migrate/20130907051527_complicate.rb
@@ -0,0 +1,12 @@
+class Complicate < ActiveRecord::Migration
+	def up
+		# something like
+		# actually not sure yet
+		
+
+		
+	end
+
+	def down
+	end
+end
---
+++ db/migrate/20130907051720_addmisc.rb
@@ -0,0 +1,8 @@
+class Addmisc < ActiveRecord::Migration
+  def up
+  	# place to set a couple users or classes other than running a rake tast
+  end
+
+  def down
+  end
+end
